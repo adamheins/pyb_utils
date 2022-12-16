7a0b6ef35b6eabe9aeb8ca9b6a66349c9ed8a0f0
Adam Heins <mail@adamheins.com>
2022-12-16 16:56:25 -0500

    Bump version to 0.2.1: switch from liegroups to spatialmath package.

--- pyb_utils/math.py
+++ pyb_utils/math.py
@@ -1,16 +1,30 @@
-from liegroups import SO3
+from spatialmath.base import q2r, r2q, qunit
 
 
-QUAT_ORDER = "xyzw"
+QUAT_ORDER = "xyzs"
 
 
-def quaternion_rotate(quat, point):
-    """Rotate a point using a unit quaternion."""
-    return SO3.from_quaternion(quat, ordering=QUAT_ORDER).dot(point)
+def quaternion_to_matrix(q):
+    """Convert quaternion q to rotation matrix."""
+    return q2r(q, order=QUAT_ORDER)
 
 
-def quaternion_multiply(quat1, quat2):
-    """Hamilton product of two quaternions stored in xyzw order."""
-    C1 = SO3.from_quaternion(quat1, ordering=QUAT_ORDER)
-    C2 = SO3.from_quaternion(quat2, ordering=QUAT_ORDER)
-    return C1.dot(C2).to_quaternion(ordering=QUAT_ORDER)
+def matrix_to_quaternion(C):
+    """Convert rotation matrix C to quaternion."""
+    return r2q(C, order=QUAT_ORDER)
+
+
+def quaternion_multiply(q0, q1, normalize=True):
+    """Hamilton product of two quaternions."""
+    order = "xyzs"
+    if normalize:
+        q0 = qunit(q0)
+        q1 = qunit(q1)
+    C0 = quaternion_to_matrix(q0)
+    C1 = quaternion_to_matrix(q1)
+    return matrix_to_quaternion(C0 @ C1)
+
+
+def quaternion_rotate(q, r):
+    """Rotate point r by rotation represented by quaternion q."""
+    return quaternion_to_matrix(q) @ r
