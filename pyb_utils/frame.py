a795b0dcb592bce68ddfe2f037e7b9894c4b313c
Austin Gregg-Smith <blooop@gmail.com>
2024-04-30 17:33:44 +0100

    Feature/debug frame (#7)
    * [feat] return the pybullet debug indices when creating a debug frame world
    
    * [feat] add type hints return lines for debug_frame
    
    * [fix] apply proper docstring format for return type information
    
    * [fix] remove extra colon

--- pyb_utils/frame.py
+++ pyb_utils/frame.py
@@ -1,10 +1,16 @@
 """This modules provides visible oriented frames for debugging the simulation."""
+
 import pybullet as pyb
 
 from .math import quaternion_rotate
 
 
-def debug_frame_world(size, origin, orientation=(0, 0, 0, 1), line_width=1):
+def debug_frame_world(
+    size: float,
+    origin: tuple[float],
+    orientation: tuple[float] = (0, 0, 0, 1),
+    line_width: float = 1,
+) -> tuple[int]:
     """Attach a frame to the world for debugging purposes.
 
     Parameters
@@ -17,32 +23,37 @@ def debug_frame_world(size, origin, orientation=(0, 0, 0, 1), line_width=1):
         A quaternion :math:`(x, y, z, w)` represented the frame's orientation.
     line_width : float
         Width of the lines that make up the frame.
+    Returns
+    -------
+    : tuple[int]
+        pybullet ids of the lines
     """
     dx = quaternion_rotate(orientation, [size, 0, 0])
     dy = quaternion_rotate(orientation, [0, size, 0])
     dz = quaternion_rotate(orientation, [0, 0, size])
 
-    pyb.addUserDebugLine(
+    l1 = pyb.addUserDebugLine(
         origin,
         list(dx + origin),
         lineColorRGB=[1, 0, 0],
         lineWidth=line_width,
     )
-    pyb.addUserDebugLine(
+    l2 = pyb.addUserDebugLine(
         origin,
         list(dy + origin),
         lineColorRGB=[0, 1, 0],
         lineWidth=line_width,
     )
-    pyb.addUserDebugLine(
+    l3 = pyb.addUserDebugLine(
         origin,
         list(dz + origin),
         lineColorRGB=[0, 0, 1],
         lineWidth=line_width,
     )
+    return l1, l2, l3
 
 
-def debug_frame(size, obj_uid, link_index):
+def debug_frame(size: float, obj_uid: int, link_index: int) -> tuple[int]:
     """Attach a frame to a link for debugging purposes.
 
     Parameters
@@ -54,25 +65,30 @@ def debug_frame(size, obj_uid, link_index):
     link_index : int
         Index of the link on the body to attach the frame to. Use `-1` for the
         base link.
+    Returns
+    -------
+    : tuple[int]
+        pybullet ids of the lines
     """
-    pyb.addUserDebugLine(
+    l1 = pyb.addUserDebugLine(
         [0, 0, 0],
         [size, 0, 0],
         lineColorRGB=[1, 0, 0],
         parentObjectUniqueId=obj_uid,
         parentLinkIndex=link_index,
     )
-    pyb.addUserDebugLine(
+    l2 = pyb.addUserDebugLine(
         [0, 0, 0],
         [0, size, 0],
         lineColorRGB=[0, 1, 0],
         parentObjectUniqueId=obj_uid,
         parentLinkIndex=link_index,
     )
-    pyb.addUserDebugLine(
+    l3 = pyb.addUserDebugLine(
         [0, 0, 0],
         [0, 0, size],
         lineColorRGB=[0, 0, 1],
         parentObjectUniqueId=obj_uid,
         parentLinkIndex=link_index,
     )
+    return l1, l2, l3
